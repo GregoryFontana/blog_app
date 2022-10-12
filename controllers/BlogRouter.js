2dedb494b730238e8e7f1232aa84ffc77f3a1d08
GregoryFontana <gregjfontana@gmail.com>
2022-10-12 14:27:30 -0400

    in class commit

--- controllers/BlogRouter.js
+++ controllers/BlogRouter.js
@@ -3,15 +3,14 @@ const BlogModel = require('../models/BlogSchema')
 
 const router = express.Router()
 
-
-// GET: ALL BLOGS
-router.get('/:id', async (req, res) => {
+// GET: All Blogs
+router.get('/', async (req, res) => {
     try {
         const blogs = await BlogModel.find({})
-        res.send(blogs)
+        res.render('Blogs/Blogs', {blogs: blogs})
     } catch (error) {
         console.log(error);
-        res.status(403).send('Cannot Get')
+        res.status(403).send('Cannot get')
     }
 })
 
@@ -26,43 +25,39 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-//POST:  CREATE a NEW BLOG
-
+// POST: CREATE a New Blog
 router.post('/', async (req, res) => {
-    try {
+    try{
         const newBlog = await BlogModel.create(req.body)
         res.send(newBlog)
-    } catch (error) {
+    } catch(error){
         console.log(error);
         res.status(403).send('Cannot create')
     }
-    
 })
 
-//  PUT:    UPDATE BY ID
-
-router.put('/:id', async (req, res) => {
-    try {
-        const updatedBlog = await BlogModel.findByIdAndUpdate(req.params.id, req.body, {'returnDocument': "after"})
-        res.send(updatedBlog)
-    } catch (error) {
+// PUT: Update By ID
+router.put('/:id', async (req, res)=> {
+   try {
+    const updatedBlog = await BlogModel.findByIdAndUpdate(req.params.id, req.body, {'returnDocument' :"after"})
+    res.send(updatedBlog)
+   } catch (error) {
         console.log(error);
         res.status(403).send('Cannot put')
-    }
-    
+   }
 })
 
 
 // DELETE
-router.delete ('/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
-        await BlogModel.findByIdAndRemove(req.params.id)
+        const deletedBlog = await BlogModel.findByIdAndRemove(req.params.id)
+        console.log(deletedBlog);
         res.send('Blog Deleted')
     } catch (error) {
         console.log(error);
-        res.status(403).send('Cannot delete')
+        res.status(403).send('Cannot put')
     }
 })
 
-
-module.exports = router
+module.exports = router;
--- models/UserSchema.js
+++ models/UserSchema.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    email: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
 }, 
 {timestamps: { createdAt: 'created_at'} })
 
-module.exports = mongoose.model('Blog', userSchema)
+module.exports = mongoose.model('User', userSchema)
